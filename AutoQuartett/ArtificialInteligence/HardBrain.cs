ea9539cd128b210aa2b6fc2150415d25b51e1190
swjinx <s.lindemeier@web.de>
2020-01-17 11:55:57 +0100

    useless usings deleted

--- AutoQuartett/ArtificialInteligence/HardBrain.cs
+++ AutoQuartett/ArtificialInteligence/HardBrain.cs
@@ -1,9 +1,7 @@
 ﻿using AutoQuartett.ArtificialInteligence.Base;
 using AutoQuartett.Cards;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 namespace AutoQuartett.ArtificialInteligence
 {
